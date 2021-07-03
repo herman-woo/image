72ce68a19e40528cb70f76fc43144576bb9ec7a5
herman-woo <72119202+herman-woo@users.noreply.github.com>
2021-07-03 00:10:04 -0400

    Updated Search
    If the thumbs folder doesnt exist, the script will create a folder and then create the new file

--- src/routes/convert/index.ts
+++ src/routes/convert/index.ts
@@ -5,25 +5,61 @@ import search from '../../utilities/search';
 import imageProcess from '../../utilities/imageProcess';
 
 const convert = express.Router();
+const checkKey = (oldValue: string, type:string): (string|number) => {
+  let newValue = parseInt(oldValue)
+  let html = ''
+  if(isNaN(newValue)){//if key entered is not a number, value will be true
+    if(oldValue === undefined){//if value is undefined, notify user
+      html += `<h5>No ${type} entered, default value used 200</h5>`
+      return html
+    }
+    else{//else a string was entered
+      html += `<h5>Invalid ${type} key, default value used = 200</h5>`
+      return html
+    }
+  }
+  else{
+    if(newValue.toString() !== oldValue){
+      html += `<h5>Mixed characters and numbers in ${type} key, default value used = 200</h5>`
+      return html
+    }
+    else{
+      return newValue
+    }
+  }
+}
+
+
 convert.get('/', search, async (req, res) => {
-  const widthdata = req.query.width as string;
-  const heightdata = req.query.height as string;
+  const widthdata = (req.query.width as unknown) as string;
+  const heightdata = (req.query.height as unknown) as string;
   const input = `src/imgs/full/${res.locals.image}`;
   const outputPath = 'src/imgs/thumbs/';
   const newFile = `${req.query.filename}.jpg`;
   const output = outputPath + newFile;
   let width = 200;
   let height = 200;
-  if (widthdata !== undefined) {
-    width = parseInt(widthdata);
+  let result = ""
+  let checkWidth = checkKey(widthdata,"width")
+  let checkHeight = checkKey(heightdata,"height")
+  //console.log(checkWidth,checkHeight)
+  const update = (checkedKey: (string|number),originalValue:number):number => {
+    if (typeof checkedKey === "number"){
+      return checkedKey
+    }
+    else{
+      result += checkedKey
+      return originalValue
+    }
   }
-  if (heightdata !== undefined) {
-    height = parseInt(heightdata);
-  }
+  width = update(checkWidth,width)
+  height = update(checkHeight,height)
+  
   await imageProcess(input, width, height, output);
   console.log('...done');
+  result += `<h2>converted - ${width}x${height}</h2><img src="http://localhost:3000/imgs/thumbs/${newFile}"></img>`
   res.send(
-    `<h2>converted - ${width}x${height}</h2><img src="http://localhost:3000/imgs/thumbs/${newFile}"></img>`
+   result
   );
 });
 
--- src/utilities/search.ts
+++ src/utilities/search.ts
@@ -8,32 +8,47 @@ const search = async (
   next: Function
 ): Promise<void> => {
   const name = req.query.filename; //get filename from req
-  if (name) {
+  if (name) {//if name is not undefined, conduct search
     let ext = 'ext';
-    if (req.query.ext === undefined) {
-      ext = '.jpg';
+    if (req.query.ext === undefined) {//if no ext was defined
+      ext = '.jpg';//set jpg as default extention
     } else {
-      ext = `.${req.query.ext}`;
+      ext = `.${req.query.ext}`;//if ext key was used, set the extention to the key
     }
-    const image = (name + ext) as string;
-    const files = await fs.readdir('src/imgs/full');
-    const result = files.find((file) => file === image);
-    if (result) {
-      res.locals.image = image;
-      const newFiles = await fs.readdir('src/imgs/thumbs');
-      const existNew = newFiles.find((file) => file === `${name}.jpg`);
-      if (existNew) {
-        console.log('A Converted', existNew, 'file already exists');
-        res.send(
-          `<div>Converted File Exists</div><img src="http://localhost:3000/imgs/thumbs/${name}.jpg"></img>`
+    const image = (name + ext) as string;//get the full file name 
+    try{
+    const files = await fs.readdir('src/imgs/full');//get an array of all existing imgs in the full folder
+    const result = files.find((file) => file === image);//search to see if the entered filename exists in the fs
+    if (result) {// if image exists
+      res.locals.image = image;//set a value to be passed on in response.locals
+      //check to see if thumbs directory exists
+      try{
+        const thumbs = await fs.readdir('src/imgs/thumbs');//check thumbs folder to see if image exists
+        const convertExists = thumbs.find((thumb) => thumb === `${name}.jpg`)
+        if (convertExists){//if the file exists in the folder exists:
+         console.log(`A Converted ${name}.jpg file already exists`);
+         res.send(//serve the existing file
+          `<div>Converted ${image} file exists</div><img src="http://localhost:3000/imgs/thumbs/${name}.jpg"></img>`
         );
-      } else {
-        console.log('Coverting...');
-        next();
+        }
+        else{
+          console.log(`Coverting ${image}...`);
+          next();//If thumbs file exists && current image name cannot be found in the folder, run convert script
+        }
       }
-    } else {
+      catch(error){//if thumbs folder does not exist:
+        await fs.mkdir('src/imgs/thumbs')//create the folder
+        const thumbs = await fs.readdir('src/imgs/thumbs')
+        console.log('Created new Thumbs folder,',`Coverting ${image}...`);
+        next();//since there was no thumbs folder, that means no thumbs existsed prior, clearing it up to be converted.
+      }
+    }
+     else{
       console.log('Search Result: No Image Found');
       res.send('Error: No Image Found');
+    }}
+    catch(error){
+      res.send("Failed to get images")
     }
   } else {
     res.send('Error: No filename requested');
